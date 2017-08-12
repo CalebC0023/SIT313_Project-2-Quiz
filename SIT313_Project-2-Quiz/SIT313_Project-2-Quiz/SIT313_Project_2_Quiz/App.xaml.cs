c257ef06531d3374b6b681d08730d2dad70b0a8d
Caleb Chan <calebc@deakin.edu.au>
2017-08-13 00:25:51 +1000

    Initial empty app

---
+++ SIT313_Project-2-Quiz/SIT313_Project-2-Quiz/SIT313_Project_2_Quiz/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SIT313_Project_2_Quiz
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SIT313_Project_2_Quiz.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ SIT313_Project-2-Quiz/SIT313_Project-2-Quiz/SIT313_Project_2_Quiz/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SIT313_Project_2_Quiz
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
